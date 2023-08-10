76730f6b6aace296504d5f4b69bb1c9632304a10
islam.bouayache <islam.bouayache@itcomp-dz.com>
2023-08-10 13:53:32 +0100

    fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     title: 'BOUAYACHE Mohamed Islam',
     description:
       'BOUAYACHE Mohamed Islam is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://islam-b.github.io', // No trailing slash allowed!
+    siteUrl: 'http://islamb.me', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
 
   },
