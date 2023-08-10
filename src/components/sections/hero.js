5ead1b5f5828f6c6c5ac3f621869377c9cd8db02
islam.bouayache <islam.bouayache@itcomp-dz.com>
2023-08-10 13:38:53 +0100

    upload resume

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -77,7 +77,7 @@ const Hero = () => {
   const five = (
     <a
       className="email-link"
-       href=""
+       href="/resume.pdf"
       target="_blank"
       rel="noreferrer">
       Download my resume
